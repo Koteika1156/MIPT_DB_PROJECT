4fcc4246f602524e6bc4aa9475c85884cc7127ee
Nikita O <89998783+Koteika1156@users.noreply.github.com>
2024-04-08 19:12:24 +0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -14,7 +14,7 @@ SELECT * FROM Employees WHERE StartDate > '2024-01-01';
 SELECT * FROM Employees WHERE FirstName = 'Иван';
 
 -- 6. Получить самый часто встречающийся материал изделий
-SELECT Material, COUNT(*) AS MaterialCount FROM Parts GROUP BY Material ORDER BY MaterialCount DESC LIMIT 1;
+SELECT Material, COUNT(*) AS MaterialCount FROM Part GROUP BY Material ORDER BY MaterialCount DESC LIMIT 1;
 
 -- 7. Получить количество станков находящихся в эксплуатации
 SELECT COUNT(*) FROM Machines WHERE DecommissionDate IS NULL;
@@ -23,7 +23,7 @@ SELECT COUNT(*) FROM Machines WHERE DecommissionDate IS NULL;
 SELECT Date FROM Deliveries GROUP BY Date ORDER BY SUM(Quantity) DESC LIMIT 1;
 
 -- 9. Получить самую легкую производимую деталь из Стали
-SELECT * FROM Parts WHERE Material = 'Сталь' ORDER BY Weight ASC LIMIT 1;
+SELECT * FROM Part WHERE Material = 'Сталь' ORDER BY Weight ASC LIMIT 1;
 
 -- 10. Получить количество производимых деталей типа "Крепеж"
-SELECT COUNT(*) FROM Parts WHERE Type = 'Крепеж';
+SELECT COUNT(*) FROM Part WHERE Type = 'Крепеж';
